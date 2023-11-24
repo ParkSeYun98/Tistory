4cb5015ac8ece55991668beccecb8112a1fa18ba
SeYun <81186461+ParkSeYun98@users.noreply.github.com>
2023-11-24 10:11:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import Parser from "rss-parser";
  * README.MD
  */
 
-[![Readme Update](https://github.com/ParkSeYun98/Tistory/actions/workflows/main.yml/badge.svg)](https://github.com/ParkSeYun98/Tistory/actions/workflows/main.yml)
 
 let text = `# Hi there 👋
 
